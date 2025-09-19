945fc0a4739260584cd31c387f3c0d309ecbf1c8
rajdeep dutta <dutta.rajdeep2022@gmail.com>
2025-09-20 02:22:49 +0530

    Add render.yaml

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import authRoutes from "./routes/authRoute.js";
 import cors from "cors";
 import categoryRoutes from "./routes/categoryRoute.js";
 import productRoutes from "./routes/productRoutes.js";
+import path from "path";
 
 //configure dotenv for taking and using dotenv data on server .js
 dotenv.config();
@@ -20,6 +21,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "./client/build")));
 
 //routes
 app.use("/api/v1/auth", authRoutes);
@@ -27,8 +29,12 @@ app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
 //rest api(ata dia amra website r home page thakbo and sata thaka response send korbo)
-app.get("/", (req, res) => {
-  res.send("<h1>welcome to ecommerce app</h1>");
+// app.get("/", (req, res) => {
+//   res.send("<h1>welcome to ecommerce app</h1>");
+// });
+
+app.use("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 //Port(jata amra kono website k call krta pari)(evn file a ja data ta a6a port r sata amra process.env.port{akna port holo name jata env ta store a6a} )
